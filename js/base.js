b3c720384992fbd87ae434e861bd06063eb4f408
procrasticoder <iamankitmishra07@gmail.com>
2021-12-09 02:46:38 +0530

    first commit

---
+++ js/base.js
@@ -0,0 +1,141 @@
+document.getElementsByTagName("header")[0].innerHTML=`
+<nav class="navbar-main navbar navbar-expand-lg navbar-light px-4 mb-0">
+<div class="container-fluid">
+  <a href="index.html"><img src="images/logo.gif" alt=""></a>
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    
+    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+      <li class="nav-item mx-3">
+        <a class="nav-link" href="index.html">Home</a>
+      </li>
+      <li class="nav-item mx-3">
+        <a class="nav-link" href="about-us.html">About Us</a>
+      </li>
+      <li class="nav-item dropdown mx-3">
+        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false" style="background-color: #db0b00;
+        border-radius: 3px; color: #fff;">Products</a>
+        <ul class="dropdown-menu">
+          <li><a class="dropdown-item"  href="">Filling Machines</a></li>
+          <li><hr class="dropdown-divider"></li>
+          <li><a class="dropdown-item" href="">Packing Machines</a></li>
+          <li><hr class="dropdown-divider"></li>
+          <li><a class="dropdown-item" href="">Metal Detectors</a></li>
+          <li><hr class="dropdown-divider"></li>
+          <li><a class="dropdown-item" href="">Industrial Checkweighers</a></li>
+          <li><hr class="dropdown-divider"></li>
+          <li><a class="dropdown-item" href="">Other Industrial Metal Detectors</a></li>
+          <li><hr class="dropdown-divider"></li>
+          <li><a class="dropdown-item" href="">More Products...</a></li>
+        </ul>
+      </li>
+      <li class="nav-item mx-3">
+        <a class="nav-link" href="contact.html">Contact Us</a>
+      </li>
+    </ul>
+  </div>
+</div>
+</nav>
+`
+document.getElementsByTagName('footer')[0].innerHTML=`
+<div class="footer_one">
+<div class="q_links">
+    <h3>
+        Quick Links
+    </h3>
+    <a href="index.html">Home</a>
+    <a href="about-us.html">About Us</a>
+    <a href="products.html">Products</a>
+    <a href="contact.html">Contact</a>
+    <div class="footerline"></div>
+</div>
+
+<div class="q_links">
+    <h3>
+        Our Products
+    </h3>
+    <a  href="products-1.html#aromatic-solvent">Industrial Conveyors</a>
+          
+          <a  href="">Filling Machines</a>
+          
+          <a href="">Packing Machines</a>
+         
+          <a href="">Metal Detectors</a>
+          
+          <a href="">Industrial Checkweighers</a>
+          
+          <a href="">Other Industrial Metal Detectors</a>
+          
+    
+    <div class="footerline"></div>
+</div>
+
+<div class="q_contact">
+    <h3>
+        Contact Us
+    </h3>
+    <div class="q_contact_link">
+        <i class="fa fa-home"></i>
+        <p><b>Goa</b><br>
+        13, Chase International,
+        P.E. Jose Vaz Road,
+        Vasco Da Gama,
+        Goa, INDIA 403802 <br>
+        <b>Mumbai</b><br> 
+        9, Ashapura Park,
+        Sector 19, Airoli, Navi Mumbai,
+        Maharashtra, INDIA 400 708
+        </p>
+    </div>
+
+    <div class="q_contact_link">
+        <i class="fa fa-phone"></i>
+        <p>+91- 9892205909<br>
+        +91- 9823309636<br>        
+        </p>
+    </div>
+    <div class="q_contact_link">
+        <i class="fa fa-envelope"></i>
+        <a href="https://www.gmail.com" style="text-decoration: underline; cursor: pointer;">labdhi.petrochem@yahoo.in</a>
+    </div>
+    <div class="footerline"></div>
+
+</div>
+
+<div class="q_follow_us">
+    <h3>
+        Follow Us
+    </h3>
+    <div class="q_follow_us_link">
+        <i class="fa fa-facebook"></i>
+        <p>facebook</p>
+    </div>
+
+    <div class="q_follow_us_link">
+        <i class="fa fa-instagram"></i>
+        <p>Instagram</p>
+    </div>
+    <div class="q_follow_us_link">
+        <i class="fa fa-youtube"></i>
+        <p>YouTube</p>
+    </div>
+    <div class="q_follow_us_link">
+        <i class="fa fa-linkedin"></i>
+        <p>Linkedln</p>
+    </div>
+    <div class="footerline"></div>
+
+</div>
+</div>
+<div class="footer_two">
+<div class="footer_content">
+    All Rights Reserved. Target Innovations (Terms of Use)<br>
+                    <a href="https://adjointindia.com/" style="color: inherit; cursor: pointer;">Powered by AdJoint India</a>
+</div>
+<div class="footer_logo">
+    <img style="cursor:pointer;cursor:hand" src="https://seal.starfieldtech.com/images/3/en/siteseal_sf_3_h_l_m.gif"  alt="SSL site seal - click to verify">
+</div>
+</div>
+`
